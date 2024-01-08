9d352f6a6b3742b8cf3c17b7400d9f02fce24f93
ziteh <honmonoh@gmail.com>
2024-01-08 20:54:37 +0800

    Add label

--- elebox/src/components/PartNew.vue
+++ elebox/src/components/PartNew.vue
@@ -13,11 +13,30 @@ async function newPart() {
 
 <template>
   <form class="row" @submit.prevent="newPart">
-    <input id="part-name-in" v-model="partName" placeholder="Name" />
-    <input id="part-qty-in" v-model="partQty" placeholder="Quantity" type="number" pattern="[0-9]*" />
-    <input id="part-type-in" v-model="partType" placeholder="Type" />
+    <div class="form-group">
+      <label for="part-name-in">Name: </label>
+      <input id="part-name-in" v-model="partName" placeholder="AMS1117" />
+    </div>
+    <div class="form-group">
+      <label for="part-qty-in">Quantity: </label>
+      <input id="part-qty-in" v-model="partQty" placeholder="15" type="number" pattern="[0-9]*" />
+    </div>
+    <div class="form-group">
+      <label for="part-type-in">Type: </label>
+      <input id="part-type-in" v-model="partType" placeholder="LDO" />
+    </div>
     <button type="submit">Add</button>
   </form>
 </template>
 
+<style>
+.row {
+  display: flex;
+  justify-content: space-between;
+}
+
+.form-group {
+  margin-right: 10px;
+}
+</style>
 
