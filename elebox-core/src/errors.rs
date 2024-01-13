4b74680ff48ddadb0df4786c64bcd477a94f42c8
ziteh <honmonoh@gmail.com>
2024-01-13 12:56:13 +0800

    Update part_types module

--- elebox-core/src/errors.rs
+++ elebox-core/src/errors.rs
@@ -3,9 +3,9 @@ use std::error::Error;
 
 #[derive(fmt::Debug)]
 pub enum EleboxError {
-    PartAlreadyExists(String),
-    PartNotExists(String),
-    PartInventoryShortage(String),
+    AlreadyExists(String),
+    NotExists(String),
+    InventoryShortage(String),
 }
 
 impl Error for EleboxError {}
@@ -13,10 +13,12 @@ impl Error for EleboxError {}
 impl fmt::Display for EleboxError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            EleboxError::PartAlreadyExists(ref name) => write!(f, "Part {} already exists", name),
-            EleboxError::PartNotExists(ref name) => write!(f, "Part {} does not exists", name),
-            EleboxError::PartInventoryShortage(ref name) => {
-                write!(f, "Part {} not enough stock", name)
+            EleboxError::AlreadyExists(ref name) => {
+                write!(f, "Item \"{}\" already exists", name)
+            }
+            EleboxError::NotExists(ref name) => write!(f, "Item \"{}\" does not exists", name),
+            EleboxError::InventoryShortage(ref name) => {
+                write!(f, "Part \"{}\" not enough stock", name)
             }
         }
     }
