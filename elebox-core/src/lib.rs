e4cf1cf52258a58c5382f4be5278ab64fd3afa20
ziteh <honmonoh@gmail.com>
2024-01-13 13:44:59 +0800

    Add part module

--- elebox-core/src/lib.rs
+++ elebox-core/src/lib.rs
@@ -1,22 +1,8 @@
-mod errors;
 mod db;
+mod errors;
+mod part;
 mod part_type;
 
-use std::{
-    collections::{hash_map::RandomState, HashMap},
-    fmt::Debug,
-    io::Read,
-    ptr,
-    str::{self, from_utf8},
-};
-
-use jammdb::{Data, KVPair, DB};
-use rmp_serde::{Deserializer, Serializer};
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
 pub use errors::*;
+pub use part::*;
 pub use part_type::*;
-
-pub mod elebox {
-}
