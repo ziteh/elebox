4e3402a4aa44d32742fce1d796c38edc2ef47011
ziteh <honmonoh@gmail.com>
2024-05-01 16:48:15 +0800

    Add some ref for new part

--- elebox-tauri/src/views/Part.vue
+++ elebox-tauri/src/views/Part.vue
@@ -10,12 +10,34 @@ interface Categories {
   }
 }
 
-const partName = ref("");
-const partQty = ref("");
-const category = ref("");
-let categories = reactive<Categories>({});
+interface Package {
+  [index: number]: {
+    name: string;
+    type: string;
+    alias: string;
+  }
+}
 
-const val = ref("");
+interface Manufacturer {
+  [index: number]: {
+    name: string;
+    alias: string;
+  }
+}
+
+const partName = ref();
+const partQty = ref();
+const category = ref();
+let categories = reactive<Categories>({});
+let packages = reactive<Package>({});
+let manufacturers = reactive<Manufacturer>({});
+
+const pkg = ref();
+const mfr = ref();
+const cost = ref();
+const location = ref();
+const alias = ref();
+const description = ref();
 
 async function newPart() {
   await invoke("part_new", { name: partName.value, qty: parseInt(partQty.value), ptype: category.value });
@@ -48,30 +70,32 @@ onMounted(getCategories);
       <v-divider class="ma-8"></v-divider>
 
       <v-row class="ga-8">
-        <v-select label="Package" :items="Object.values(categories).map(cat => cat.name)" variant="outlined"></v-select>
-        <v-select label="Manufacturer" :items="Object.values(categories).map(cat => cat.name)"
-          variant="outlined"></v-select>
-        <v-text-field label="Cost" variant="outlined" v-model="val"></v-text-field>
-        <v-text-field label="Location" variant="outlined" v-model="val" placeholder="Box #1"></v-text-field>
+        <v-select label="Package" :items="Object.values(packages).map(pck => pck.name)" variant="outlined"
+          v-model="pkg"></v-select>
+        <v-select label="Manufacturer" :items="Object.values(manufacturers).map(mfr => mfr.name)" variant="outlined"
+          v-model="mfr"></v-select>
+        <v-text-field label="Cost" variant="outlined" v-model="cost"></v-text-field>
+        <v-text-field label="Location" variant="outlined" v-model="location" placeholder="Box #1"></v-text-field>
       </v-row>
       <v-row class="ga-8">
-        <v-text-field label="Alias" variant="outlined" v-model="val" placeholder=""></v-text-field>
-        <v-text-field label="Mfr #" variant="outlined" v-model="val" placeholder="SC0914(7)"
+        <v-text-field label="Alias" variant="outlined" v-model="alias" placeholder=""></v-text-field>
+        <v-text-field label="Mfr #" variant="outlined" v-model="pkg" placeholder="SC0914(7)"
           title="Manufacturer #"></v-text-field>
-        <v-text-field label="Mouser #" variant="outlined" v-model="val" placeholder="358-SC09147"></v-text-field>
-        <v-text-field label="Digi-Key #" variant="outlined" v-model="val"
+        <v-text-field label="Mouser #" variant="outlined" v-model="pkg" placeholder="358-SC09147"></v-text-field>
+        <v-text-field label="Digi-Key #" variant="outlined" v-model="pkg"
           placeholder="2648-SC0914(7)CT-ND"></v-text-field>
       </v-row>
       <v-row class="ga-8">
-        <v-text-field label="Product Url" variant="outlined" v-model="val" placeholder="https://"></v-text-field>
-        <v-text-field label="Datasheet" variant="outlined" v-model="val" placeholder="https://"></v-text-field>
-        <v-text-field label="Image" variant="outlined" v-model="val" placeholder=""></v-text-field>
+        <v-text-field label="Product Url" variant="outlined" v-model="pkg" placeholder="https://"></v-text-field>
+        <v-text-field label="Datasheet" variant="outlined" v-model="pkg" placeholder="https://"></v-text-field>
+        <v-text-field label="Image" variant="outlined" v-model="pkg" placeholder=""></v-text-field>
       </v-row>
       <v-row class="ga-8">
-        <v-textarea label="Description" variant="outlined" v-model="val" placeholder="Write something ..."></v-textarea>
+        <v-textarea label="Description" variant="outlined" v-model="description"
+          placeholder="Write something ..."></v-textarea>
       </v-row>
       <v-row class="ga-8">
-        <v-textarea label="Suppliers" variant="outlined" v-model="val" placeholder=""></v-textarea>
+        <v-textarea label="Suppliers" variant="outlined" v-model="pkg" placeholder=""></v-textarea>
       </v-row>
     </v-container>
   </v-form>
