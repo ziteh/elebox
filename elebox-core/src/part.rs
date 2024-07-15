6984f9d1848712ce1d30cf2326f518bc783f41f4
ziteh <honmonoh@gmail.com>
2024-07-15 19:42:11 +0800

    refactor(core): PartManager

--- elebox-core/src/part.rs
+++ elebox-core/src/part.rs
@@ -1,4 +1,4 @@
-use crate::{csv::*, db::*, errors::EleboxError, TreeNode};
+use crate::{csv::*, db::*, errors::EleboxError};
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
@@ -53,77 +53,72 @@ impl<'a> PartManager<'a> {
     }
 
     pub fn delete(&self, name: &str) -> Result<(), EleboxError> {
-        let id = self.db.get_part_id(name);
-        if id.is_none() {
-            return Err(EleboxError::NotExists(name.to_string()));
-        }
+        let id = self
+            .db
+            .get_part_id(name)
+            .ok_or(EleboxError::NotExists(name.to_string()))?;
 
-        self.db.delete_part(&id.unwrap());
+        self.db.delete_part(&id);
         return Ok(());
     }
 
+    fn to_db_part(&self, part: &Part) -> Result<DbPart, EleboxError> {
+        if self.db.get_part_id(&part.name).is_some() {
+            return Err(EleboxError::AlreadyExists(part.name.to_string()));
+        }
+
+        let category_id = match self.db.get_category_id(&part.category) {
+            Some(id) => id.to_string(),
+            None => return Err(EleboxError::NotExists("category".to_string())),
+        };
+
+        let package_id = match &part.package {
+            Some(name) => match self.db.get_package_id(&name) {
+                Some(id) => id,
+                None => return Err(EleboxError::NotExists(name.to_string())),
+            },
+            None => "".to_string(),
+        };
+
+        let mfr_id = match &part.mfr {
+            Some(name) => match self.db.get_mfr_id(&name) {
+                Some(id) => id,
+                None => return Err(EleboxError::NotExists(name.to_string())),
+            },
+            None => "".to_string(),
+        };
+
+        let unwrap_or_empty = |opt: &Option<String>| opt.as_deref().unwrap_or("").to_string();
+
+        let db_part = DbPart {
+            name: part.name.to_string(),
+            quantity: part.quantity,
+            category_id,
+            mfr_id,
+            package_id,
+            package_detail: unwrap_or_empty(&part.package_detail),
+            alias: unwrap_or_empty(&part.alias),
+            description: unwrap_or_empty(&part.description),
+            location: unwrap_or_empty(&part.location),
+            mfr_no: unwrap_or_empty(&part.mfr_no),
+            datasheet_link: unwrap_or_empty(&part.datasheet_link),
+            product_link: unwrap_or_empty(&part.product_link),
+            image_link: unwrap_or_empty(&part.image_link),
+            custom_fields: part.custom_fields.clone(),
+            suppliers: part.suppliers.clone(),
+        };
+
+        Ok(db_part)
+    }
+
     pub fn update(&self, name: &str, new_part: &Part) -> Result<(), EleboxError> {
-        let id = self.db.get_part_id(name);
-        if id.is_none() {
+        if self.db.get_part_id(name).is_none() {
             return Err(EleboxError::NotExists(name.to_string()));
         }
 
-        if self.db.get_part_id(&new_part.name).is_some() {
-            return Err(EleboxError::AlreadyExists(new_part.name.to_string()));
-        }
-
-        let category_id = match self.db.get_category_id(&new_part.category) {
-            Some(id) => id.to_string(),
-            None => "none".to_string(), // TODO: empty value
-        };
-
-        let package_id = match &new_part.package {
-            Some(n) => match self.db.get_package_id(&n) {
-                Some(id) => id,
-                None => return Err(EleboxError::NotExists(n.to_string())),
-            },
-            None => "".to_string(),
-        };
-
-        let mfr_id = match &new_part.mfr {
-            Some(n) => match self.db.get_mfr_id(&n) {
-                Some(id) => id,
-                None => return Err(EleboxError::NotExists(n.to_string())),
-            },
-            None => "".to_string(),
-        };
-
-        let empty = &"".to_string();
-
-        let alias = new_part.alias.as_ref().unwrap_or(empty);
-        let package_detail = new_part.package_detail.as_ref().unwrap_or(empty);
-        let description = new_part.description.as_ref().unwrap_or(empty);
-        let location = new_part.location.as_ref().unwrap_or(empty);
-        let mfr_no = new_part.mfr_no.as_ref().unwrap_or(empty);
-        let datasheet_link = new_part.datasheet_link.as_ref().unwrap_or(empty);
-        let product_link = new_part.product_link.as_ref().unwrap_or(empty);
-        let image_link = new_part.image_link.as_ref().unwrap_or(empty);
-
-        let db_part = DbPart {
-            name: new_part.name.to_string(),
-            quantity: new_part.quantity,
-            category_id,
-            package_id,
-            package_detail: package_detail.to_string(),
-            mfr_id,
-            alias: alias.to_string(),
-            description: description.to_string(),
-            location: location.to_string(),
-            mfr_no: mfr_no.to_string(),
-            datasheet_link: datasheet_link.to_string(),
-            product_link: product_link.to_string(),
-            image_link: image_link.to_string(),
-            custom_fields: new_part.custom_fields.clone(),
-            suppliers: new_part.suppliers.clone(),
-        };
-
+        let db_part = self.to_db_part(new_part)?;
         self.db.update_part(name, &db_part);
-        return Ok(());
+        Ok(())
     }
 
     pub fn update_part_quantity(&self, name: &str, quantity: i16) -> Result<(), EleboxError> {
@@ -145,136 +140,62 @@ impl<'a> PartManager<'a> {
     }
 
     pub fn add(&self, part: &Part) -> Result<(), EleboxError> {
-        if self.db.get_part_id(&part.name).is_some() {
-            return Err(EleboxError::AlreadyExists(part.name.to_string()));
-        }
-
-        let category_id = match self.db.get_category_id(&part.category) {
-            Some(id) => id.to_string(),
-            None => "none".to_string(), // TODO: empty value
-        };
-
-        let package_id = match &part.package {
-            Some(n) => match self.db.get_package_id(&n) {
-                Some(id) => id,
-                None => return Err(EleboxError::NotExists(n.to_string())),
-            },
-            None => "".to_string(),
-        };
-
-        let mfr_id = match &part.mfr {
-            Some(n) => match self.db.get_mfr_id(&n) {
-                Some(id) => id,
-                None => return Err(EleboxError::NotExists(n.to_string())),
-            },
-            None => "".to_string(),
-        };
-
-        let empty = &"".to_string();
-
-        let alias = part.alias.as_ref().unwrap_or(empty);
-        let package_detail = part.package_detail.as_ref().unwrap_or(empty);
-        let description = part.description.as_ref().unwrap_or(empty);
-        let location = part.location.as_ref().unwrap_or(empty);
-        let mfr_no = part.mfr_no.as_ref().unwrap_or(empty);
-        let datasheet_link = part.datasheet_link.as_ref().unwrap_or(empty);
-        let product_link = part.product_link.as_ref().unwrap_or(empty);
-        let image_link = part.image_link.as_ref().unwrap_or(empty);
-
-        let db_part = DbPart {
-            name: part.name.to_string(),
-            quantity: part.quantity,
-            category_id,
-            package_id,
-            package_detail: package_detail.to_string(),
-            mfr_id,
-            alias: alias.to_string(),
-            description: description.to_string(),
-            location: location.to_string(),
-            mfr_no: mfr_no.to_string(),
-            datasheet_link: datasheet_link.to_string(),
-            product_link: product_link.to_string(),
-            image_link: image_link.to_string(),
-            custom_fields: part.custom_fields.clone(),
-            suppliers: part.suppliers.clone(),
-        };
-
+        let db_part = self.to_db_part(part)?;
         self.db.add_part(&db_part);
-        return Ok(());
+        Ok(())
     }
 
-    pub fn get(&self, name: &str) -> Result<Part, EleboxError> {
-        let id = self.db.get_part_id(name);
-        if id.is_none() {
-            return Err(EleboxError::NotExists(name.to_string()));
-        }
-
-        let db_part = self.db.get_part_from_id(&id.unwrap()).unwrap();
-        let category = match self.db.get_category_from_id(&db_part.category_id) {
-            Some(pt) => pt.name,
-            None => "none".to_string(),
-        };
+    fn from_db_part(&self, db_part: DbPart) -> Result<Part, EleboxError> {
+        let category = self
+            .db
+            .get_category_from_id(&db_part.category_id)
+            .ok_or(EleboxError::NotExists("category".to_string()))?
+            .name;
 
         let package = self
             .db
             .get_package_from_id(&db_part.package_id)
-            .map(|p| p.name);
+            .map(|pkg| pkg.name);
 
-        let mfr = self.db.get_mfr_from_id(&db_part.mfr_id).map(|p| p.name);
+        let mfr = self.db.get_mfr_from_id(&db_part.mfr_id).map(|mfr| mfr.name);
 
-        let mut part = Part::new(&db_part.name, &category, db_part.quantity);
-        part.package = package;
-        part.package_detail = Some(db_part.package_detail);
-        part.mfr = mfr;
-        part.alias = Some(db_part.alias);
-        part.description = Some(db_part.description);
-        part.location = Some(db_part.location);
-        part.mfr_no = Some(db_part.mfr_no);
-        part.datasheet_link = Some(db_part.datasheet_link);
-        part.product_link = Some(db_part.product_link);
-        part.image_link = Some(db_part.image_link);
-        part.custom_fields = db_part.custom_fields;
-        part.suppliers = db_part.suppliers;
+        let part = Part {
+            name: db_part.name,
+            category,
+            quantity: db_part.quantity,
+            package,
+            package_detail: Some(db_part.package_detail),
+            mfr,
+            alias: Some(db_part.alias),
+            description: Some(db_part.description),
+            location: Some(db_part.location),
+            mfr_no: Some(db_part.mfr_no),
+            datasheet_link: Some(db_part.datasheet_link),
+            product_link: Some(db_part.product_link),
+            image_link: Some(db_part.image_link),
+            custom_fields: db_part.custom_fields,
+            suppliers: db_part.suppliers,
+        };
 
         return Ok(part);
     }
 
+    pub fn get(&self, name: &str) -> Result<Part, EleboxError> {
+        let id = self
+            .db
+            .get_part_id(name)
+            .ok_or(EleboxError::NotExists(name.to_string()))?;
+
+        let db_part = self.db.get_part_from_id(&id).unwrap();
+        self.from_db_part(db_part)
+    }
+
     pub fn list(&self) -> Vec<Part> {
-        let db_parts = self.db.get_parts();
-        let mut parts: Vec<Part> = Vec::new();
-
-        for db_part in db_parts {
-            let category = match self.db.get_category_from_id(&db_part.category_id) {
-                Some(pt) => pt.name,
-                None => "none".to_string(),
-            };
-
-            let package = self
-                .db
-                .get_package_from_id(&db_part.package_id)
-                .map(|p| p.name);
-
-            let mfr = self.db.get_mfr_from_id(&db_part.mfr_id).map(|p| p.name);
-
-            let mut part = Part::new(&db_part.name, &category, db_part.quantity);
-            part.package = package;
-            part.mfr = mfr;
-
-            // TODO None if empty
-            part.package_detail = Some(db_part.package_detail);
-            part.alias = Some(db_part.alias);
-            part.description = Some(db_part.description);
-            part.location = Some(db_part.location);
-            part.mfr_no = Some(db_part.mfr_no);
-            part.datasheet_link = Some(db_part.datasheet_link);
-            part.product_link = Some(db_part.product_link);
-            part.image_link = Some(db_part.image_link);
-            part.custom_fields = db_part.custom_fields;
-            part.suppliers = db_part.suppliers;
-
-            parts.push(part);
-        }
-        return parts;
+        self.db
+            .get_parts()
+            .into_iter()
+            .filter_map(|db_p| self.from_db_part(db_p).ok())
+            .collect()
     }
 
     pub fn export_csv(&self, filename: &str) -> Result<(), ()> {
