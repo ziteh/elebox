5e3434f39be6fde00ff8eb708e673d087f5695a7
ziteh <honmonoh@gmail.com>
2024-07-18 19:07:17 +0800

    feat(tauri): make custom field and supplier as form

--- elebox-tauri/src/components/PartSupplier.vue
+++ elebox-tauri/src/components/PartSupplier.vue
@@ -38,51 +38,60 @@ function emitAdd() {
 </script>
 
 <template>
-  <v-text-field
-    label="Name"
-    variant="outlined"
-    v-model.trim="supplier.name"
-    placeholder=""
-    :rules="[(v: any) => !!v || 'Required']"
-    required
-    :readonly="!props.create"
-  ></v-text-field>
-  <v-text-field
-    label="Link"
-    variant="outlined"
-    v-model.trim="supplier.link"
-    placeholder="https://"
-    :readonly="!props.create"
-  ></v-text-field>
-  <v-text-field
-    label="Price"
-    variant="outlined"
-    v-model.number="supplier.price"
-    type="number"
-    min="0"
-    :readonly="!props.create"
-  ></v-text-field>
-  <v-text-field
-    label="Note"
-    variant="outlined"
-    v-model.trim="supplier.note"
-    placeholder=""
-    :readonly="!props.create"
-  ></v-text-field>
+  <v-form @submit.prevent>
+    <v-row class="ma-2 ga-8">
+      <v-text-field
+        label="Name"
+        variant="outlined"
+        v-model.trim="supplier.name"
+        placeholder=""
+        :rules="[(v: any) => !!v || 'Required']"
+        required
+        :readonly="!props.create"
+      ></v-text-field>
+      <v-text-field
+        label="Link"
+        variant="outlined"
+        v-model.trim="supplier.link"
+        placeholder="https://"
+        :readonly="!props.create"
+        :dirty="!props.create"
+      ></v-text-field>
+      <v-text-field
+        label="Price"
+        variant="outlined"
+        v-model.number="supplier.price"
+        type="number"
+        min="0"
+        :readonly="!props.create"
+        :dirty="!props.create"
+      ></v-text-field>
+      <v-text-field
+        label="Note"
+        variant="outlined"
+        v-model.trim="supplier.note"
+        placeholder=""
+        :readonly="!props.create"
+        :dirty="!props.create"
+      ></v-text-field>
 
-  <v-btn
-    v-if="!props.create"
-    density="comfortable"
-    icon="mdi-trash-can-outline"
-    title="Delete"
-    @click="emitDel()"
-  ></v-btn>
-  <v-btn
-    v-if="props.create"
-    density="comfortable"
-    icon="mdi-plus"
-    title="Add"
-    color="green"
-    @click="emitAdd()"
-  ></v-btn>
+      <v-btn
+        v-if="props.create"
+        density="comfortable"
+        icon="mdi-plus"
+        title="Add"
+        color="green"
+        type="submit"
+        @click="emitAdd()"
+      ></v-btn>
+      <v-btn
+        v-else
+        density="comfortable"
+        icon="mdi-trash-can-outline"
+        title="Delete"
+        type="submit"
+        @click="emitDel()"
+      ></v-btn>
+    </v-row>
+  </v-form>
 </template>
