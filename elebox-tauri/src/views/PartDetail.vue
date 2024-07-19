54c92ece1e3aea147e68b0d4b8882ed44e0c043e
ziteh <honmonoh@gmail.com>
2024-07-19 21:13:37 +0800

    feat(tauri): add confirm delete dialog for part

--- elebox-tauri/src/views/PartDetail.vue
+++ elebox-tauri/src/views/PartDetail.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import "../styles.css";
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { DbPart } from "../db_cmd_part";
 import ItemEditButton from "../components/ItemEditButton.vue";
 import PartDel from "../components/PartDel.vue";
 
+const router = useRouter();
 const route = useRoute();
 const name = ref();
 const part = ref<DbPart.Part>({
@@ -21,6 +22,11 @@ async function getPart(name: string) {
   const partData = await DbPart.get(name);
   part.value = partData as DbPart.Part;
   console.log(partData);
+}
+
+async function removePart() {
+  await DbPart.remove(name.value);
+  router.replace("/"); // Back to home
 }
 
 onMounted(() => {
@@ -39,8 +45,47 @@ onMounted(() => {
         <v-col cols="1">
           <ItemEditButton :path_name="'update_part'" :item_name="name" />
         </v-col>
-        <v-col cols="2">
-          <PartDel :part="name" />
+        <v-col cols="1">
+          <v-dialog max-width="500">
+            <template v-slot:activator="{ props: activatorProps }">
+              <v-btn
+                v-bind="activatorProps"
+                density="comfortable"
+                icon="mdi-trash-can-outline"
+                title="Delete"
+              ></v-btn>
+            </template>
+
+            <template v-slot:default="{ isActive }">
+              <v-card
+                title="Confirm Delete"
+                prepend-icon="mdi-trash-can-outline"
+              >
+                <v-card-text>
+                  <p>
+                    Are you sure you want to delete this part
+                    <strong>{{ part.name }}</strong> ?
+                  </p>
+                  <p>This action cannot be undone.</p>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    text="Cancel"
+                    @click="isActive.value = false"
+                    variant="tonal"
+                  ></v-btn>
+                  <v-btn
+                    text="Delete"
+                    @click="removePart"
+                    color="red"
+                    variant="tonal"
+                  ></v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
         </v-col>
       </v-row>
     </div>
@@ -92,23 +137,25 @@ onMounted(() => {
         <tr>
           <td>Manufacturer #</td>
           <td>
-            {{ part.mfr_no }}
-            <a
-              title="Search on Mouser"
-              target="_blank"
-              :href="`https://www.mouser.com/c/?q=${part.mfr_no}`"
-              ><v-icon>mdi-search-web</v-icon></a
-            ><a
-              title="Search on Digikey"
-              target="_blank"
-              :href="`https://www.digikey.com/en/products/result?keywords=${part.mfr_no}`"
-              ><v-icon>mdi-search-web</v-icon></a
-            ><a
-              title="Search on Octopart"
-              target="_blank"
-              :href="`https://octopart.com/search?q=${part.mfr_no}&currency=USD&specs=0`"
-              ><v-icon>mdi-search-web</v-icon></a
-            >
+            <div v-if="part.mfr_no">
+              {{ part.mfr_no }}
+              <a
+                title="Search on Mouser"
+                target="_blank"
+                :href="`https://www.mouser.com/c/?q=${part.mfr_no}`"
+                ><v-icon>mdi-search-web</v-icon></a
+              ><a
+                title="Search on Digikey"
+                target="_blank"
+                :href="`https://www.digikey.com/en/products/result?keywords=${part.mfr_no}`"
+                ><v-icon>mdi-search-web</v-icon></a
+              ><a
+                title="Search on Octopart"
+                target="_blank"
+                :href="`https://octopart.com/search?q=${part.mfr_no}&currency=USD&specs=0`"
+                ><v-icon>mdi-search-web</v-icon></a
+              >
+            </div>
           </td>
         </tr>
         <tr>
