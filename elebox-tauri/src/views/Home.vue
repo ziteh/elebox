7c8b0ce97520b34be0da5ff219ad1cbb1cf2d4a0
ziteh <honmonoh@gmail.com>
2024-07-20 12:42:26 +0800

    refactor(tauri): part list

--- elebox-tauri/src/views/Home.vue
+++ elebox-tauri/src/views/Home.vue
@@ -1,91 +1,16 @@
 <script setup lang="ts">
 import { onMounted, ref, reactive } from "vue";
-import { Part } from "../interface";
 import { useRouter, useRoute } from "vue-router";
-import { DbPart } from "../db_cmd_part";
-import PartQty from "../components/PartQty.vue";
+import PartList from "../components/PartList.vue";
 
-let parts = reactive<Part[]>([]);
-
-const router = useRouter();
-const route = useRoute();
-
-const search = ref("");
-const headers = ref([
-  { key: "name", title: "Name", sortable: true },
-  { key: "quantity", title: "Quantity" },
-  { key: "category", title: "Category" },
-  { key: "package", title: "Package" },
-  { key: "mfr", title: "Manufacturer" },
-]);
-
-async function getParts() {
-  let promise = await DbPart.list();
-  Object.assign(parts, promise);
-}
-
-function reload() {
-  // window.location.reload();
-
-  const currentPath = route.path;
-  router.replace("/redirect").then(() => {
-    router.replace(currentPath);
-  });
-}
-
-onMounted(getParts);
+// onMounted();
 </script>
 
 <template>
   <v-container>
     <v-row>
       <v-container>
-        <v-card flat variant="text">
-          <template v-slot:text>
-            <v-row class="align-center">
-              <v-col>
-                <v-text-field
-                  v-model="search"
-                  label="Search"
-                  prepend-inner-icon="mdi-magnify"
-                  variant="outlined"
-                  hide-details
-                  single-line
-                ></v-text-field>
-              </v-col>
-              <v-col cols="auto">
-                <v-btn
-                  @click="reload"
-                  text="Update"
-                  icon="mdi-refresh"
-                  density="comfortable"
-                  size="large"
-                  title="Refresh"
-                ></v-btn>
-              </v-col>
-            </v-row>
-          </template>
-
-          <v-data-table
-            :headers="headers"
-            :items="parts"
-            :search="search"
-            v-if="parts.length > 0"
-          >
-            <template v-slot:item.name="{ item }">
-              <v-btn
-                :to="{ name: 'part_detail', params: { name: item.name } }"
-                variant="text"
-              >
-                {{ item.name
-                }}<v-icon v-if="item.starred" color="#fcba03">mdi-star</v-icon>
-              </v-btn>
-            </template>
-            <template v-slot:item.quantity="{ item }">
-              <PartQty :part="item.name" />
-            </template>
-          </v-data-table>
-        </v-card>
+        <PartList />
       </v-container>
     </v-row>
   </v-container>
