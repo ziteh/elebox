df5004caff20e1808e1ae79b6a168d4187b46d3a
ziteh <honmonoh@gmail.com>
2024-07-23 20:52:20 +0800

    refactor(tauri): add '@' as `src/` alias

--- elebox-tauri/src/main.ts
+++ elebox-tauri/src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router.js";
-import i18n from "./plugins/i18n.js";
-import vuetify from "./plugins/vuetify.js";
-import "./styles.css";
+import App from "@/App.vue";
+import router from "@/router";
+import i18n from "@/plugins/i18n";
+import vuetify from "@/plugins/vuetify";
+import "@/styles.css";
 
 const app = createApp(App);
 app.use(router);
