838ec547689e5153a986f32e509d75fd772387d9
ziteh <honmonoh@gmail.com>
2024-07-24 09:06:46 +0800

    refactor(tauri): create part detail components

---
+++ elebox-tauri/src/components/PartDetail/SupplierTable.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Supplier } from "@/types/part";
+
+const props = defineProps<{
+  suppliers: Supplier[];
+}>();
+</script>
+
+<template>
+  <v-label class="mt-8">{{ $t("suppliers") }}</v-label>
+  <v-table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Link</th>
+        <th>Price</th>
+        <th>Note</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="s in props.suppliers" :key="s.name">
+        <td>{{ s.name }}</td>
+        <td>
+          <a v-if="s.link" :href="s.link" target="_blank" :title="s.link">
+            <v-icon> mdi-open-in-new </v-icon>
+          </a>
+        </td>
+        <td>{{ s.price == undefined ? "" : s.price.toFixed(3) }}</td>
+        <td>{{ s.note }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
--- elebox-tauri/src/views/PartDetail.vue
+++ elebox-tauri/src/views/PartDetail.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { DbPart } from "../utils/db_cmd_part";
-import { invoke } from "@tauri-apps/api/tauri";
-import ItemEditButton from "../components/ItemEditButton.vue";
-import ItemDeleteButton from "../components/ItemDeleteButton.vue";
+import { useRoute } from "vue-router";
+import { DbPart as Db } from "@/utils/db_cmd_part";
+import Bar from "@/components/PartDetail/Bar.vue";
+import MainTable from "@/components/PartDetail/MainTable.vue";
+import CustomFieldTable from "@/components/PartDetail/CustomFieldTable.vue";
+import SupplierTable from "@/components/PartDetail/SupplierTable.vue";
 
-const router = useRouter();
 const route = useRoute();
-const name = ref();
-const part = ref<DbPart.Part>({
+
+const current = ref<Db.Part>({
   name: "",
   quantity: 0,
   category: "",
@@ -17,208 +17,27 @@ const part = ref<DbPart.Part>({
   suppliers: [],
   starred: false,
 });
-const default_path = ref("");
 
-async function getDefaultPath() {
-  default_path.value = await invoke("get_assets_path", {});
-}
-
-const image_link = ref("");
-
-async function getPart(name: string) {
-  const partData = await DbPart.get(name);
-  part.value = partData as DbPart.Part;
-  console.log(partData);
-
-  image_link.value = part.value.image_link ?? "";
-  if (!image_link.value.startsWith("http")) {
-    image_link.value = "/@fs/" + default_path.value + image_link.value;
-  }
-}
-
-async function removePart() {
-  await DbPart.remove(name.value);
-  router.replace("/"); // Back to home
+async function fetchCurrent(name: string) {
+  const data = await Db.get(name);
+  current.value = data as Db.Part;
+  console.debug(data);
 }
 
 onMounted(() => {
-  name.value = route.params.name;
-  getDefaultPath();
-  getPart(name.value);
+  if (Array.isArray(route.params.name)) {
+    fetchCurrent(route.params.name[0]);
+  } else {
+    fetchCurrent(route.params.name);
+  }
 });
 </script>
 
 <template>
-  <v-container v-if="name">
-    <div>
-      <v-row>
-        <v-col>
-          <v-btn :to="{ name: 'home' }">Back</v-btn>
-        </v-col>
-        <v-col cols="auto">
-          <ItemEditButton :path_name="'update_part'" :item_name="name" />
-        </v-col>
-        <v-col cols="auto">
-          <ItemDeleteButton :name="part.name" @delete="removePart" />
-        </v-col>
-      </v-row>
-    </div>
-
-    <v-divider class="my-4"></v-divider>
-
-    <v-table>
-      <thead>
-        <tr>
-          <th>{{ $t("name") }}</th>
-          <th>
-            {{ part.name
-            }}<v-icon v-if="part.starred" color="#fcba03" title="Starred" end
-              >mdi-star</v-icon
-            >
-          </th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr>
-          <td>{{ $t("category") }}</td>
-          <td>{{ part.category }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("quantity") }}</td>
-          <td>{{ part.quantity }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("alias") }}</td>
-          <td>{{ part.alias }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("location") }}</td>
-          <td>{{ part.location }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("package") }}</td>
-          <td>{{ part.package }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("package_detail") }}</td>
-          <td>{{ part.package_detail }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("description") }}</td>
-          <td>{{ part.description }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("manufacturer") }}</td>
-          <td>{{ part.mfr }}</td>
-        </tr>
-        <tr>
-          <td>{{ $t("manufacturer_no") }}</td>
-          <td>
-            <div v-if="part.mfr_no">
-              {{ part.mfr_no }}
-              <a
-                title="Search on Mouser"
-                target="_blank"
-                :href="`https://www.mouser.com/c/?q=${part.mfr_no}`"
-                ><v-icon>mdi-search-web</v-icon></a
-              ><a
-                title="Search on Digikey"
-                target="_blank"
-                :href="`https://www.digikey.com/en/products/result?keywords=${part.mfr_no}`"
-                ><v-icon>mdi-search-web</v-icon></a
-              ><a
-                title="Search on Octopart"
-                target="_blank"
-                :href="`https://octopart.com/search?q=${part.mfr_no}&currency=USD&specs=0`"
-                ><v-icon>mdi-search-web</v-icon></a
-              >
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>{{ $t("datasheet") }}</td>
-          <td>
-            <a target="_blank" :href="part.datasheet_link">{{
-              part.datasheet_link
-            }}</a>
-          </td>
-        </tr>
-        <tr>
-          <td>{{ $t("product_link") }}</td>
-          <td>
-            <a target="_blank" :href="part.product_link">{{
-              part.product_link
-            }}</a>
-          </td>
-        </tr>
-        <tr>
-          <td>{{ $t("image") }}</td>
-          <td>
-            <a
-              v-if="image_link"
-              target="_blank"
-              :href="image_link"
-              :title="image_link"
-            >
-              <v-img :src="image_link" height="250"></v-img>
-            </a>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
-
-    <v-label class="mt-8">{{ $t("custom_fields") }}</v-label>
-    <v-table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Value</th>
-          <th>Field Type</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-for="cf in part.custom_fields" :key="cf.name">
-          <td>{{ cf.name }}</td>
-          <td>
-            <a
-              v-if="cf.field_type == 'Link'"
-              :href="cf.value"
-              target="_blank"
-              :title="cf.value"
-              ><v-icon> mdi-open-in-new </v-icon></a
-            >
-            <div v-else>
-              {{ cf.value }}
-            </div>
-          </td>
-          <td>{{ cf.field_type }}</td>
-        </tr>
-      </tbody>
-    </v-table>
-
-    <v-label class="mt-8">{{ $t("suppliers") }}</v-label>
-    <v-table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Link</th>
-          <th>Price</th>
-          <th>Note</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <tr v-for="s in part.suppliers" :key="s.name">
-          <td>{{ s.name }}</td>
-          <td>
-            <a :href="s.link" target="_blank" :title="s.link">Link</a>
-          </td>
-          <td>{{ s.price == undefined ? "" : s.price.toFixed(3) }}</td>
-          <td>{{ s.note }}</td>
-        </tr>
-      </tbody>
-    </v-table>
+  <v-container v-if="current.name">
+    <Bar :name="current.name" />
+    <MainTable :current="current" />
+    <CustomFieldTable :custom_fields="current.custom_fields" />
+    <SupplierTable :suppliers="current.suppliers" />
   </v-container>
 </template>
