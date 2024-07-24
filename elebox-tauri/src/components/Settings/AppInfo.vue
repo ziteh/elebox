6964d454e5307b72f3b7f95fc2f087d0f372ddff
ziteh <honmonoh@gmail.com>
2024-07-24 13:38:52 +0800

    feat(tauri): fetch app version from Tauri config by API

--- elebox-tauri/src/components/Settings/AppInfo.vue
+++ elebox-tauri/src/components/Settings/AppInfo.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { getVersion } from "@tauri-apps/api/app";
 
 const assets_dir = ref("");
+const app_version = ref("");
 
 async function fetchAssetsDir() {
   assets_dir.value = await invoke("get_assets_path", {});
 }
 
+async function getAppVersion() {
+  app_version.value = await getVersion();
+}
+
 onMounted(() => {
   fetchAssetsDir();
+  getAppVersion();
 });
 </script>
 
@@ -28,7 +35,7 @@ onMounted(() => {
       <v-label>Version</v-label>
     </v-col>
     <v-col cols="auto">
-      <code>1.0.0-beta.4</code>
+      <code>{{ app_version }}</code>
     </v-col>
   </v-row>
 </template>
