e80031699149d6c4f4969cd94b1ebe7dcffdfe54
ziteh <honmonoh@gmail.com>
2024-08-04 11:30:34 +0800

    test(core): add unit test for Mfr add()

--- elebox-core/src/manufacturer.rs
+++ elebox-core/src/manufacturer.rs
@@ -126,3 +126,81 @@ impl Transferable for ManufacturerHandler<'_> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::DbError;
+    use mockall::{mock, predicate::*};
+
+    mock! {
+        pub MyDatabase {}
+
+        impl Database<DbManufacturer> for MyDatabase {
+            fn init(&self) -> Result<(), DbError>;
+            fn get_id(&self, name: &str) -> Result<String, DbError>;
+            fn get(&self, id: &str) -> Result<DbManufacturer, DbError>;
+            fn list(&self) -> Result<Vec<DbManufacturer>, DbError>;
+            fn add(&self, item: &DbManufacturer) -> Result<(), DbError>;
+            fn update(&self, ori_id: &str, new_item: &DbManufacturer) -> Result<(), DbError>;
+            fn delete(&self, id: &str) -> Result<(), DbError>;
+            fn check(&self) -> Result<(), DbError>;
+        }
+    }
+
+    #[test]
+    fn test_add_no_existing() {
+        const SYMBOL: &str = r"_=+-!@#$%^&*()|{}/\'`~";
+
+        let test_cases = vec![
+            ("Test1", Some("TestAlias"), Some("https://test.com")),
+            ("Test2", None, Some("https://test.com")),
+            ("Test3", Some("TestAlias"), None),
+            ("Test4", None, None),
+            ("Test Space", Some("Test Alias"), Some("https://test.com")),
+            (SYMBOL, Some(SYMBOL), Some(SYMBOL)),
+            ("中文名稱", Some("中文別名"), Some("中文網址")),
+        ];
+
+        for (name, alias, url) in test_cases {
+            test_add_no_existing_value(
+                name.to_string(),
+                alias.map(|s| s.to_string()),
+                url.map(|s| s.to_string()),
+            );
+        }
+    }
+
+    fn test_add_no_existing_value(name: String, alias: Option<String>, url: Option<String>) {
+        // Arrange
+        let mfr = Manufacturer::new(
+            &name.clone(),
+            alias.clone().as_deref(),
+            url.clone().as_deref(),
+        );
+
+        let mut mock_db = MockMyDatabase::new();
+
+        // Mock get_id() always return NotExists indicating the item does not existing
+        mock_db
+            .expect_get_id()
+            .returning(|_| Err(DbError::NotExists("ID".to_string())));
+
+        // Mock add() to check all field and return Ok(())
+        mock_db
+            .expect_add()
+            .withf(move |db_mfr: &DbManufacturer| {
+                db_mfr.name == name
+                    && db_mfr.alias == alias.as_deref().unwrap_or("")
+                    && db_mfr.url == url.as_deref().unwrap_or("")
+            })
+            .returning(|_| Ok(()));
+
+        // Act
+        let handler = ManufacturerHandler { db: &mock_db };
+        let result = handler.add(&mfr);
+
+        // Assert
+        assert!(result.is_ok());
+    }
+}
